722f9d2e252e2bd4d309946d96b861721110bd6f
Seth Davenport <seth@rangle.io>
2017-04-02 22:11:01 -0400

    Toolchain updates
    Angular 4
    Angular-redux 6.1

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -2,7 +2,7 @@ import { browser, element, by } from 'protractor';
 
 describe('QuickStart E2E Tests', function () {
 
-  let expectedMsg = 'Hello @angular-redux/store!';
+  let expectedMsg: any = 'Hello @angular-redux/store!';
 
   beforeEach(function () {
     browser.get('');
